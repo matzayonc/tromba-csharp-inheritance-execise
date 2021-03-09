bcec6439bc772d96cb435e285c39129298c0eee1
Zayonc <60236390+matzayonc@users.noreply.github.com>
2021-03-09 17:55:23 +0100

    Add project files.

---
+++ Settlement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tromba
+{
+    class Settlement
+    {
+        public string name { get; };
+
+        public Settlement()
+        {
+            name = "Unnamed";
+        }
+        public Settlement(string nameOfSettlement)
+        {
+            name = nameOfSettlement;
+        }
+
+        public void welcome()
+        {
+            Console.WriteLine("Welcome in " + name + '!');
+        }
+    }
+}
---
+++ Town.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tromba
+{
+    class Town: Settlement
+    {
+        int population;
+        Town(string name, int numberOfPeople): base(name)
+        {
+            population = numberOfPeople;
+        }
+
+        public void census()
+        {
+            Console.WriteLine("In the city of " + base.name + " live " + population + "people.");
+        }
+    }
+}
